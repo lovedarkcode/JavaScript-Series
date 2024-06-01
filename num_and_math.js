9528ff662f984e09b0586863b4caff6eb22e8643
Aditya <139672646+lovedarkcode@users.noreply.github.com>
2024-06-01 11:06:12 +0530

    Add files via upload

---
+++ num_and_math.js
@@ -0,0 +1,20 @@
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(typeof balance);
+console.log(balance.toFixed(2));
+
+const num = 23.567 
+console.log(num.toPrecision(3));  // it rounds off
+
+const number = 100000000
+console.log(number.toLocaleString("en-IN"));
+
+
+// ****************************Math**************************
+
+console.log(Math);
+console.log(Math.round(4.4));
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
